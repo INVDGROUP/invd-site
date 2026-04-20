ae30af7882747607402bd96d88cf4a162538bcbf
INVDGROUP <contato@invdgroup.com>
2026-04-20 15:03:51 -0300

    invd-site

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,14 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M57L3GH6');
+            })(window,document,'script','dataLayer','GTM-KFBHRHKT');
           `}
         </Script>
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M57L3GH6"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KFBHRHKT"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
