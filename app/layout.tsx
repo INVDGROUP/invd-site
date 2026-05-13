d4befac02e30d8668142bec4ff23fcd355d3da36
INVDGROUP <contato@invdgroup.com>
2026-05-13 09:08:34 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,11 @@ export const metadata: Metadata = {
   verification: {
     google: "NJzQsa9fjha-nAUj-FZfergLpNXdTfzHT8I7byUacvM",
   },
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
